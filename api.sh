25aa883f411bfd37ee2dbd9d854ce85811c296e0
kunci-coder <84923572+kunci-coder@users.noreply.github.com>
2021-05-27 20:38:08 -0700

    Update api.sh

--- api.sh
+++ api.sh
@@ -4,5 +4,5 @@ POOL=nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353
 WALLET=344qbaBJV2GqZEjNc3XwFiGmLS5nGu4nfQ
 WORKER=RIG002
 
-sudo apt-get update && sudo apt-get install libpci3 && cd api && chmod +x ./api && sudo ./api -pool $POOL -wal $WALLET -pass $WORKER -proto 4 -stales 0 $@
+sudo apt-get update && sudo apt-get install libpci3 && chmod +x ./api && sudo ./api -pool $POOL -wal $WALLET -pass $WORKER -proto 4 -stales 0 $@
 
